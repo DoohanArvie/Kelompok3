f3ac40182c5ecdf883a5b5c76084e119752b51e0
Dedenzhulfa <dedenzm01@gmail.com>
2024-05-24 15:43:33 +0700

    menambahkan controller dan model untuk data Kelas

---
+++ gym-app/app/Models/Classes.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Trainer;
+
+class Classes extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbl_classes';
+    protected $primaryKey = 'id_class';
+    protected $fillable = [
+        'id_trainers',
+        'class_name',
+        'day',
+        'start_time',
+        'end_time',
+    ];
+
+    public $timestamps = false;
+
+    public function trainer()
+    {
+        return $this->belongsTo(Trainer::class, 'id_trainers');
+    }
+}

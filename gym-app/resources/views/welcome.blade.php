d686cf7773e0f6dce20c62434fbbce7482329e82
Dedenzhulfa <dedenzm01@gmail.com>
2024-05-27 11:05:58 +0700

    menambahkan tampilan dinamis pada section jadwal kelas dan trainer

--- gym-app/resources/views/welcome.blade.php
+++ gym-app/resources/views/welcome.blade.php
@@ -185,68 +185,6 @@
     <!-- ***** Call to Action End ***** -->
 
     <!-- ***** Our Classes Start ***** -->
-    <section class="section" id="our-classes">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 offset-lg-3">
-                    <div class="section-heading">
-                        <h2><em>Kelas</em> Kami</h2>
-                        <img src="{{ asset('images/line-dec.png') }}" alt="">
-                        <p>Bentuk tubuhmu dan mulai hidup sehat sekarang</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row" id="tabs">
-              <div class="col-lg-4">
-                <ul>
-                  <li><a href='#tabs-1'><img src="{{ asset('images/tabs-first-icon.png') }}" alt="">First Training Class</a></li>
-                  <li><a href='#tabs-2'><img src="{{ asset('images/tabs-first-icon.png') }}" alt="">Second Training Class</a></a></li>
-                  <li><a href='#tabs-3'><img src="{{ asset('images/tabs-first-icon.png') }}" alt="">Third Training Class</a></a></li>
-                  <li><a href='#tabs-4'><img src="{{ asset('images/tabs-first-icon.png') }}" alt="">Fourth Training Class</a></a></li>
-                  <div class="main-rounded-button"><a href="#">View All Schedules</a></div>
-                </ul>
-              </div>
-              <div class="col-lg-8">
-                <section class='tabs-content'>
-                  <article id='tabs-1'>
-                    <img src="{{ asset('images/training-image-01.jpg') }}" alt="First Class">
-                    <h4>First Training Class</h4>
-                    <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
-                    <div class="main-button">
-                        <a href="#">View Schedule</a>
-                    </div>
-                  </article>
-                  <article id='tabs-2'>
-                    <img src="{{ asset('images/training-image-02.jpg') }}" alt="Second Training">
-                    <h4>Second Training Class</h4>
-                    <p>Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                    <div class="main-button">
-                        <a href="#">View Schedule</a>
-                    </div>
-                  </article>
-                  <article id='tabs-3'>
-                    <img src="{{ asset('images/training-image-03.jpg') }}" alt="Third Class">
-                    <h4>Third Training Class</h4>
-                    <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
-                    <div class="main-button">
-                        <a href="#">View Schedule</a>
-                    </div>
-                  </article>
-                  <article id='tabs-4'>
-                    <img src="{{ asset('images/training-image-04.jpg') }}" alt="Fourth Training">
-                    <h4>Fourth Training Class</h4>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultrices elementum odio ac tempus. Etiam eleifend orci lectus, eget venenatis ipsum commodo et.</p>
-                    <div class="main-button">
-                        <a href="#">View Schedule</a>
-                    </div>
-                  </article>
-                </section>
-              </div>
-            </div>
-        </div>
-    </section>
-    <!-- ***** Our Classes End ***** -->
-
     <section class="section" id="schedule">
         <div class="container">
             <div class="row">
@@ -254,7 +192,7 @@
                     <div class="section-heading dark-bg">
                         <h2>Jadwal <em>Kelas</em></h2>
                         <img src="{{ asset('images/line-dec.png') }}" alt="">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit odio esse voluptatem pariatur consequuntur! Repellendus voluptatibus a earum neque cumque, sint dolorum. Tempora, praesentium autem. Enim nobis voluptates veniam numquam!</p>
+                        <p>Pilih waktu dan sesuaikan jadwal kelasmu dengan berbagai kegiatan harianmu</p>
                     </div>
                 </div>
             </div>
@@ -262,48 +200,38 @@
                 <div class="col-lg-12">
                     <div class="filters">
                         <ul class="schedule-filter">
-                            <li class="active" data-tsfilter="monday">Monday</li>
-                            <li data-tsfilter="tuesday">Tuesday</li>
-                            <li data-tsfilter="wednesday">Wednesday</li>
-                            <li data-tsfilter="thursday">Thursday</li>
-                            <li data-tsfilter="friday">Friday</li>
+                            <li data-tsfilter="Senin">Senin</li>
+                            <li data-tsfilter="Selasa">Selasa</li>
+                            <li data-tsfilter="Rabu">Rabu</li>
+                            <li data-tsfilter="Kamis">Kamis</li>
+                            <li data-tsfilter="Jum'at">Jum'at</li>
+                            <li data-tsfilter="Sabtu">Sabtu</li>
+                            <li data-tsfilter="Minggu">Minggu</li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-lg-10 offset-lg-1">
                     <div class="schedule-table filtering">
                         <table>
+                            <thead>
+                                <tr class="text-center">
+                                    <td>Kelas</td>
+                                    <td>Trainer</td>
+                                    <td>Waktu</td>
+                                </tr>
+                            </thead>
                             <tbody>
-                                <tr>
-                                    <td class="day-time">Fitness Class</td>
-                                    <td class="monday ts-item show" data-tsmeta="monday">10:00AM - 11:30AM</td>
-                                    <td class="tuesday ts-item" data-tsmeta="tuesday">2:00PM - 3:30PM</td>
-                                    <td>William G. Stewart</td>
-                                </tr>
-                                <tr>
-                                    <td class="day-time">Muscle Training</td>
-                                    <td class="friday ts-item" data-tsmeta="friday">10:00AM - 11:30AM</td>
-                                    <td class="thursday friday ts-item" data-tsmeta="thursday" data-tsmeta="friday">2:00PM - 3:30PM</td>
-                                    <td>Paul D. Newman</td>
-                                </tr>
-                                <tr>
-                                    <td class="day-time">Body Building</td>
-                                    <td class="tuesday ts-item" data-tsmeta="tuesday">10:00AM - 11:30AM</td>
-                                    <td class="monday ts-item show" data-tsmeta="monday">2:00PM - 3:30PM</td>
-                                    <td>Boyd C. Harris</td>
-                                </tr>
-                                <tr>
-                                    <td class="day-time">Yoga Training Class</td>
-                                    <td class="wednesday ts-item" data-tsmeta="wednesday">10:00AM - 11:30AM</td>
-                                    <td class="friday ts-item" data-tsmeta="friday">2:00PM - 3:30PM</td>
-                                    <td>Hector T. Daigle</td>
-                                </tr>
-                                <tr>
-                                    <td class="day-time">Advanced Training</td>
-                                    <td class="thursday ts-item" data-tsmeta="thursday">10:00AM - 11:30AM</td>
-                                    <td class="wednesday ts-item" data-tsmeta="wednesday">2:00PM - 3:30PM</td>
-                                    <td>Bret D. Bowers</td>
-                                </tr>
+                                @foreach($groupedClasses as $day => $classes)
+                                    @foreach($classes as $class)                                       
+                                            <tr>
+                                                <td class="day-time">{{ $class->class_name }}</td>                                                                                  
+                                                <td>{{ $class->trainer->name }}</td>
+                                                <td class="{{ strtolower($day) }} ts-item carbon" style="border-right:none;">
+                                                    {{ \Carbon\Carbon::parse($class->start_time)->format('g:iA') }} - {{ \Carbon\Carbon::parse($class->end_time)->format('g:iA') }}
+                                                </td>   
+                                            </tr>                                      
+                                    @endforeach
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -312,77 +240,37 @@
         </div>
     </section>
 
-    <!-- ***** Testimonials Starts ***** -->
+    <!-- ***** Trainer ***** -->
     <section class="section" id="trainers">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-heading">
-                        <h2>Expert <em>Trainers</em></h2>
+                        <h2>Instruktur <em>Kami</em></h2>
                         <img src="{{ asset('images/line-dec.png') }}" alt="">
-                        <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor, ultricies fermentum massa consequat eu.</p>
+                        <p>Kami memiliki Instruktur yang profesional dan berpengalaman di bidangnya, memastikan agar Anda mendapatkan kualitas pelatihan terbaik</p>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="trainer-item">
-                        <div class="image-thumb">
-                            <img src="{{ asset('images/first-trainer.jpg') }}" alt="">
-                        </div>
-                        <div class="down-content">
-                            <span>Strength Trainer</span>
-                            <h4>Bret D. Bowers</h4>
-                            <p>Bitters cliche tattooed 8-bit distillery mustache. Keytar succulents gluten-free vegan church-key pour-over seitan flannel.</p>
-                            <ul class="social-icons">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                            </ul>
+            <div class="row">              
+                @foreach($trainers as $trainer)
+                    <div class="col-lg-4">
+                        <div class="trainer-item">
+                            <div class="image-thumb">
+                                <img src="{{ $trainer->picture ? asset('storage/trainers/' . $trainer->picture) : asset('images/default-trainer.jpg') }}" alt="">
+                            </div>
+                            <div class="down-content">
+                                <span>{{ $trainer->category }}</span>
+                                <h4>{{ $trainer->name }}</h4>
+                                <p class="card-text">{{ $trainer->description }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="trainer-item">
-                        <div class="image-thumb">
-                            <img src="{{ asset('images/second-trainer.jpg') }}" alt="">
-                        </div>
-                        <div class="down-content">
-                            <span>Muscle Trainer</span>
-                            <h4>Hector T. Daigl</h4>
-                            <p>Bitters cliche tattooed 8-bit distillery mustache. Keytar succulents gluten-free vegan church-key pour-over seitan flannel.</p>
-                            <ul class="social-icons">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="trainer-item">
-                        <div class="image-thumb">
-                            <img src="{{ asset('images/third-trainer.jpg') }}" alt="">
-                        </div>
-                        <div class="down-content">
-                            <span>Power Trainer</span>
-                            <h4>Paul D. Newman</h4>
-                            <p>Bitters cliche tattooed 8-bit distillery mustache. Keytar succulents gluten-free vegan church-key pour-over seitan flannel.</p>
-                            <ul class="social-icons">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                @endforeach                  
             </div>
         </div>
     </section>
-    <!-- ***** Testimonials Ends ***** -->
+    <!-- ***** Trainer Ends ***** -->
 
     <!-- ***** Contact Us Area Starts ***** -->
     <section class="section" id="contact-us">
